9ac73a06a944d29f1f7e5cf8f823fd1e56943c3a
Somya <isomya13@gmail.com>
2023-09-04 16:28:41 +0530

    [ADD]: added caching

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ const PORT = process.env.PORT || 5000;
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
 
 const client = redis.createClient(REDIS_PORT);
+await client.connect();
 
 const app= express();
 const url="https://api.github.com/users/"
+
+function setResponse(username, repos){
+  return `<h2>${username} has ${repos} repos</h2>`;
+}
 
 async function getRepos(req,res, next){
    try {
@@ -17,14 +22,31 @@ async function getRepos(req,res, next){
     const response= await fetch (`${url}${username}`);
     const data = await response.json();
     const repos= data.public_repos;
-    res.send(data);
+
+    //cache
+    client.setEx(username,3600,repos);
+    res.send(setResponse(username, repos));
    } catch (error) {
-    console.log(err);
+    console.log(error);
     res.status(500)
    }
 }
 
-app.get('/repos/:username', getRepos);
+//cache middleware
+
+function cache(req, res, next){
+  const {username} = req.params;
+   
+  client.get(username, (err, data)=>{
+   if(err) throw err;
+   if(data !==null){
+    res.send(setResponse(username, data));
+   }
+   else next();
+  });
+}
+
+app.get('/repos/:username', cache, getRepos);
 
 app.listen(PORT, ()=> {
   console.log("App is listening");
